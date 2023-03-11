22669b697afb607c86dc4d09636676deda8cc1bd
fengjianying <2787752537@qq.com>
2023-03-11 22:28:25 +0800

    第一次提交

---
+++ cmake-build-debug/CMakeFiles/c_pr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_pr.dir/main.c.obj"
+  "c_pr.exe"
+  "c_pr.exe.manifest"
+  "c_pr.pdb"
+  "libc_pr.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_pr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
